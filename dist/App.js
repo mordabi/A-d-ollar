067c782598c5a0252c75d1e755ac66d777997892
mor <morex48562@gmail.com>
2018-10-07 21:55:07 +0300

    express configuration file - App class

---
+++ dist/App.js
@@ -0,0 +1,46 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const cors = require("cors");
+//import HeroRouter from './routes/HeroRouter';
+const mongoose = require("mongoose");
+const ParticipateRoute_1 = require("./routes/ParticipateRoute");
+var config = require('./config').get(process.env.NODE_ENV);
+const MONGO_URI = config.database;
+// Creates and configures an ExpressJS web server.
+class App {
+    //Run configuration methods on the Express instance.
+    constructor() {
+        this.express = express();
+        this.middleware();
+        this.routes();
+        mongoose.connect(MONGO_URI || process.env.MONGODB_URI);
+    }
+    // Configure Express middleware.
+    middleware() {
+        this.express.use(logger('dev'));
+        this.express.use(bodyParser.json());
+        this.express.use(bodyParser.urlencoded({ extended: false }));
+        this.express.use(helmet());
+        this.express.use(cors());
+    }
+    // Configure API endpoints.
+    routes() {
+        /* This is just to get up and running, and to make sure what we've got is
+         * working so far. This function will change when we start to add more
+         * API endpoints */
+        let router = express.Router();
+        // placeholder route handler
+        router.get('/', (req, res, next) => {
+            res.json({
+                message: 'Hello World!'
+            });
+        });
+        this.express.use('/', router);
+        this.express.use('/participate', ParticipateRoute_1.default);
+    }
+}
+exports.default = new App().express;
---
+++ src/App.ts
@@ -0,0 +1,55 @@
+import * as path from 'path';
+import * as express from 'express';
+import * as logger from 'morgan';
+import * as bodyParser from 'body-parser';
+import * as helmet from 'helmet';
+import * as cors from 'cors';
+//import HeroRouter from './routes/HeroRouter';
+import * as mongoose  from 'mongoose';
+import participate from './routes/ParticipateRoute';
+var config = require('./config').get(process.env.NODE_ENV);
+
+
+const MONGO_URI =config.database;
+// Creates and configures an ExpressJS web server.
+class App {
+
+  // ref to Express instance
+  public express: express.Application;
+
+  //Run configuration methods on the Express instance.
+  constructor() {
+    this.express = express();
+    this.middleware();
+    this.routes();
+    mongoose.connect(MONGO_URI || process.env.MONGODB_URI);
+  }
+
+  // Configure Express middleware.
+  private middleware(): void {
+    this.express.use(logger('dev'));
+    this.express.use(bodyParser.json());
+    this.express.use(bodyParser.urlencoded({ extended: false }));
+    this.express.use(helmet());
+    this.express.use(cors());
+  }
+
+  // Configure API endpoints.
+  private routes(): void {
+    /* This is just to get up and running, and to make sure what we've got is
+     * working so far. This function will change when we start to add more
+     * API endpoints */
+    let router = express.Router();
+    // placeholder route handler
+    router.get('/', (req, res, next) => {
+      res.json({
+        message: 'Hello World!'
+      });
+    });
+    this.express.use('/', router);
+    this.express.use('/participate',participate);
+  }
+
+}
+
+export default new App().express;
